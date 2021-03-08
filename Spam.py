9c40729b3bbd405c83bf98893613cd03db127357
L-Varun <73778208+L-Varun@users.noreply.github.com>
2021-03-08 19:27:34 +0530

    Add files via upload

---
+++ Spam.py
@@ -0,0 +1,19 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import yaml
+
+info = yaml.load(open('spam.yml'))
+url = info["whatsapp"]["url"]
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+temp = input()
+if temp != "" :
+    driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
+    temp_1 = input("message : ")
+for i in range(5) :
+    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(temp_1)
+    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
