fa1e46a8351805106b17a960e3e800e6afd527bb
L-Varun <73778208+L-Varun@users.noreply.github.com>
2021-03-08 19:34:43 +0530

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -6,6 +6,7 @@ import yaml
 
 info = yaml.load(open('spam.yml'))
 url = info["whatsapp"]["url"]
+spam_number = info["whatsapp"]["spam_number"]
 driver = webdriver.Chrome()
 
 driver.get(url)
@@ -14,6 +15,6 @@ temp = input()
 if temp != "" :
     driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
     temp_1 = input("message : ")
-for i in range(5) :
+for i in range(spam_number) :
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(temp_1)
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
