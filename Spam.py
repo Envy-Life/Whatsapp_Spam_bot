ac76ddf632fb41d740595903fc8f5f2b7655a0e7
L-Varun <73778208+L-Varun@users.noreply.github.com>
2021-03-08 19:38:48 +0530

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,7 +1,4 @@
-import selenium
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import yaml
 
 info = yaml.load(open('spam.yml'))
@@ -11,10 +8,9 @@ driver = webdriver.Chrome()
 
 driver.get(url)
 
-temp = input()
-if temp != "" :
-    driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
-    temp_1 = input("message : ")
+
+driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
+temp_1 = input("message : ")
 for i in range(spam_number) :
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(temp_1)
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
